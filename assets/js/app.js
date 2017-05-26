54356c9c051fd4aa0c8f41c0f1dfc8f8fa9913d4
Ayda Sulca <suni_yuri_94@hotmail.com>
2017-05-26 13:37:57 -0500

    avance-2

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,12 +17,12 @@ function initMap(){
           }
         }
 
-  var latitud, longitud;
+  var latitud, longitud,miUbicacion;
   var funcionExito = function(posicion) {
           latitud = posicion.coords.latitude;
           longitud = posicion.coords.longitude;
 
-  var miUbicacion = new google.maps.Marker({
+  miUbicacion = new google.maps.Marker({
             position: {lat:latitud, lng:longitud},
             map:map
 
@@ -37,5 +37,44 @@ function initMap(){
         }
 
         document.getElementById('encuentrame').addEventListener("click", buscar);
+
+        var inputPartida = document.getElementById('input-partida');
+        var inputDestino = document.getElementById('input-destino');
+
+        new google.maps.places.Autocomplete(inputPartida);
+        new google.maps.places.Autocomplete(inputDestino);
+
+        var directionsService = new google.maps.DirectionsService;
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        var calculateAndDisplayRoute = function (DirectionsService, directionsDisplay){
+          directionsService.route({
+            origin: inputPartida.value,
+            destination: inputDestino.value,
+            travelMode: 'DRIVING'
+          }, function(response, status){
+            if (status === 'OK') {
+              var distancia =
+               Number((response.routes[0].legs[0].distance.text.replace("km","")).replace(",","."));
+              var costo = distancia*1.75;
+              if (costo<4) {
+                tarifa.innerHTML="S/.4";
+              }
+              tarifa.innerHTML= "S/. " + parseInt(costo);
+              console.log(response.routes[0].legs[0].distance.text);
+              directionsDisplay.setDirections(response);
+              miUbicacion.setMap(null);
+              markadorLaboratoria.setMap(null);
+            }else {
+              window.alert("No encontramos una ruta.");
+            }
+          });
+        }
+
+        directionsDisplay.setMap(map);
+        var trazarRuta = function(){
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+        };
+        document.getElementById('trazar-ruta').addEventListener("click", trazarRuta);
+
 
 };
